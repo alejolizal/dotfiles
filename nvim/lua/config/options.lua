abab0ee6a56406b27a622a4168dcb49d7c3b9fb9
alejandro liz <alejandro.liz@sii.cl>
2026-01-08 13:20:57 +0000

    feat(nvim): configurar clipboard del sistema
    - Agregar vim.opt.clipboard = 'unnamedplus'
    - Permite copiar/pegar entre Neovim y otras aplicaciones
    - Usa xclip para integración con portapapeles del sistema

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -1,3 +1,7 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+-- Configuración del portapapeles del sistema
+-- Permite copiar/pegar entre Neovim y otras aplicaciones
+vim.opt.clipboard = "unnamedplus" -- Usa el portapapeles del sistema (+) para todas las operaciones de yank/paste
